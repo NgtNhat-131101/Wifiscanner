ded31b398f093dc26e5b99b02a146f37d4a0476b
= <nguyentiennhat2k1@gmail.com>
2023-07-19 01:13:46 +0700

    vercel

--- webclient/settings.py
+++ webclient/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-28fpd5d%k0in70k0m^47k(ny1ls74cy8j%!c8*vet*+$3zh2y3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', "127.0.0.1"]
 
 
 
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
